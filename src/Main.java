8e987ebdedf012347fc12ce6a9a3875ea2894871
Gabriel-2A <gaa.021205@gmail.com>
2022-05-26 11:52:07 -0500

    Avance 6:

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 import java.util.ArrayList;
 
-public class Main {
+public class Main implements UtilInterface{
+
+    static ArrayList<Habilidad> habilidades = new ArrayList<>();
+    static Habilidad habilidad = new Habilidad("Golpe hierro", "Lanza un poderoso ataque capaz de destruir rocas", 57);
+    static Habilidad habilidad2 = new Habilidad("Vuelo", "Vuela muy alto para despues tirar un golpe fulminante", 78);
+    static Habilidad habilidad3 = new Habilidad("Confusion", "Ataca directamente a la mente ", 65);
+    static ArrayList<String> tiposPokemon;
     public static void main(String[] args){
 
             }
