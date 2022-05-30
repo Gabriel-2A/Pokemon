4fbd63de19a3473d825a3a14a62aad2e2aa21b7c
Gabriel-2A <gaa.021205@gmail.com>
2022-05-30 11:40:43 -0500

    Avance 6:

--- src/Main.java
+++ src/Main.java
@@ -14,31 +14,21 @@ public class Main implements UtilInterface{
 
     @Override
     public void tiempoJugado(long tiempoInicial, long tiempoFinal) {
-        tiempoInicial = System.currentTimeMillis();
-        Thread.sleep(2000);
-        tiempoFinal = System.currentTimeMillis();
+      //  tiempoInicial = System.currentTimeMillis();
+        //Thread.sleep(2000);
+      //  tiempoFinal = System.currentTimeMillis();
         double tiempo =(double) ((tiempoFinal - tiempoInicial)/1000);
-        return tiempo;
+        //return tiempo;
     }
 
     @Override
     public void mostrarMenu() {
         Scanner sc = new Scanner(System.in);
-        System.out.println("1.-" +
-                            "2.-" +
-                            "3.-" +
-                            "4.-");
-        int decicion = sc.nextInt();
-        switch (decicion){
-            case 1:
-                break;
-            case 2:
-                break;
-            case 3:
-                break;
-            case 4:
-                break;
-        }
+        System.out.println("1.- explorar" +
+                            "2.- entrar a  la tienda" +
+                            "3.- ver estaisticas" +
+                            "4.- tiempo jugador" +
+                            "5.- terminar");
     }
 
     @Override
@@ -57,21 +47,56 @@ public class Main implements UtilInterface{
         switch (tipo){
             case "Agua":
                 debilYFuerte[0] = "Planta";
-                debilYFuerte
+                debilYFuerte[1] = "Tierra";
                 break;
             case "Tierra":
+                debilYFuerte[0] = "Agua";
+                debilYFuerte[1] = "Fuego";
                 break;
             case "Fuego":
+                debilYFuerte[0] = "Tierra";
+                debilYFuerte[1] = "Planta";
                 break;
             case "Planta":
+                debilYFuerte[0] = "Fuego";
+                debilYFuerte[1] = "Agua";
                 break;
         }
-        return new String[0];
+        return debilYFuerte;
     }
 
     @Override
     public String[] nombresPokemonAleatorios(String tipo) {
-        switch (tipo){}
-        return new String[0];
+        String[] nombres = new String[4];
+        switch (tipo){
+            case "Agua":
+                nombres[0] = "Squirtle";
+                nombres[1] = "Magicarp";
+                nombres[2] = "Mudkip";
+                nombres[3] = "Tentacool";
+                break;
+
+            case "Tierra":
+                nombres[0] = "Onix";
+                nombres[1] = "Agroon";
+                nombres[2] = "Geodude";
+                nombres[3] = "Flygon";
+                break;
+
+            case "Fuego":
+                nombres[0] = "Charizard";
+                nombres[1] = "Slamander";
+                nombres[2] = "Chameroth";
+                nombres[3] = "Kombuzken";
+                break;
+
+            case "Planta":
+                nombres[0] = "Bulbasaur";
+                nombres[1] = "Brelooom";
+                nombres[2] = "Gardevoir";
+                nombres[3] = "Treko";
+                break;
+        }
+        return nombres;
     }
 }
